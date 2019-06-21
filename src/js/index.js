aad6f015e1bf30b18dac11ea9bbb0982649f22c9
madcido <fabowow@gmail.com>
2019-06-21 10:35:20 -0300

    Remove commented parts from code

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,6 @@
-// import '../styles/main.css';
 import ProjectList from './components/ProjectList';
 import NewProjectButton from './components/NewProjectButton';
 import { PrevButton, NextButton } from './components/Slider';
-
-// alert('6');
 
 (function init() {
   const container = document.querySelector(".container");
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,4 @@
 const path = require('path');
-// const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './src/js/index.js',
@@ -8,13 +7,4 @@ module.exports = {
     filename: 'bundle.js',
     publicPath: path.resolve(__dirname, 'dist'),
     }
-  // },
-  // module:{
-  //   rules:[
-  //     {
-  //       test:/\.css$/i,
-  //       use:['style-loader','css-loader']
-  //     },
-  //   ]
-  // }
 };
